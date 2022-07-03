a0c8c4b6c40ab7f108a3b335eb1bfab01853802a
JMHerrera <bohemtus@gmail.com>
2022-07-03 00:39:06 -0400

    home, login y contacto completamente funcionales

--- manager/core/urls.py
+++ manager/core/urls.py
@@ -1,10 +1,21 @@
 from django.urls import path
+from . import views
 from .views import index, login, contacto, page01
 
 urlpatterns =[
     path('', index, name='index'),
     path('login/', login, name='login'),
     path('contacto/', contacto, name='contacto'),
-    path('zapatillas/', page01, name='page01'),
-
+    path('zapatillas/', views.page01, name='page01'),
+    path('zapatillas/', views.page01, name='page02'),
+    path('zapatillas/', views.page01, name='page03'),
+    path('zapatillas/', views.page01, name='page04'),
+    path('zapatillas/', views.page01, name='page05'),
+    path('zapatillas/', views.page01, name='page06'),
+    path('zapatillas/', views.page01, name='page07'),
+    path('zapatillas/', views.page01, name='page08'),
+    path('zapatillas/', views.page01, name='page09'),
+    path('zapatillas/', views.page01, name='page10'),
+    path('zapatillas/', views.page01, name='page11'),
+    path('zapatillas/', views.page01, name='page12'),
 ]
--- manager/core/views.py
+++ manager/core/views.py
@@ -13,4 +13,37 @@ def contacto(request):
     return render(request, 'contacto.html')
 
 def page01(request):
-    return render(request, '1.html')
+    return render(request, 'html/1.html')
+
+def page02(request):
+    return render(request, 'html/2.html')
+    
+def page03(request):
+    return render(request, 'html/3.html')
+
+def page04(request):
+    return render(request, 'html/4.html')
+
+def page05(request):
+    return render(request, 'html/5.html')
+
+def page06(request):
+    return render(request, 'html/6.html')
+
+def page07(request):
+    return render(request, 'html/7.html')
+
+def page08(request):
+    return render(request, 'html/8.html')
+
+def page09(request):
+    return render(request, 'html/9.html')
+
+def page10(request):
+    return render(request, 'html/10.html')
+
+def page11(request):
+    return render(request, 'html/11.html')
+
+def page12(request):
+    return render(request, 'html/12.html')
